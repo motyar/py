bea603d27f2251ce0b8cff1b9ee73064fddebb96
Motyar <dharmmotyar@gmail.com>
2021-07-14 22:25:39 +0530

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,10 +1,21 @@
 import json
-import datetime
 
-def endpoint(params):
-    current_time = datetime.datetime.now().time()
-    name = params.get("name", "stranger")
+
+def hello(event, context):
     body = {
-        "message": "Hello " + name + ", the current time is " + str(current_time)
+        "message": "Go Serverless v2.0! Your function executed successfully!",
+        "input": event,
     }
-    return body
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY
+    # integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
