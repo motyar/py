81710a08cc10ffcd0e1c250eca651bf5e19a92d8
Motyar <dharmmotyar@gmail.com>
2021-07-15 21:00:56 +0530

    Update handler.py

--- handler.py
+++ handler.py
@@ -1,15 +1,7 @@
 from fastapi import FastAPI
-from mangum import Mangum  # <---------- import Mangum library
 
-from api.v1.api import router as api_router
+app = FastAPI(title="MyAwesomeApp")
 
-app = FastAPI(title='Serverless Lambda FastAPI')
-
-app.include_router(api_router, prefix="/api/v1")
-
-
-@app.get("/",  tags=["Endpoint Test"])
-def main_endpoint_test():
-    return {"message": "Welcome CI/CD Pipeline with GitHub Actions!"}
-
-handler = Mangum(app=app) # <----------- wrap the API with Mangum
+@app.get("/")
+def handler():
+    return {"message": "Hello World"}
