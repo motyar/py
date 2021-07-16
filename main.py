8029d78d3fc64f6a27d060c314c139bc5db87ce9
Motyar <dharmmotyar@gmail.com>
2021-07-16 21:01:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-import os
-
 from fastapi import FastAPI
-from mangum import Mangum
 
-stage = os.environ.get('STAGE', None)
-openapi_prefix = f"/{stage}" if stage else "/"
-
-app = FastAPI(title="MyAwesomeAppX", openapi_prefix=openapi_prefix) # Here is the magic
+app = FastAPI(title="MyAwesomeApp")
 
 @app.get("/hello")
 def hello_world():
-    return {"message": "Hello World!!"}
-
-
-handler = Mangum(app)
+    return {"message": "Hello World"}
